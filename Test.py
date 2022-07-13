ba3a39605528f0272239de0cd95bf9bca2ab989b
Seaofpurpleclouds <107317259+Seaofpurpleclouds@users.noreply.github.com>
2022-07-13 20:49:11 +0300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,2 +1,82 @@
-python
-print ('hello world')
+def check(n):
+    if (n < 2):  # compare number
+        return (n % 2 == 0)
+    return (check(n - 2))   # function call from a number minus 2
+
+
+n = input('Введите число: ')  # please enter a number to check
+if n.isdigit() == False:   # check if the string consisted of numbers
+    print('Вы ввели не целое число!')
+else:
+    n = int(n)
+    if (check(n) == True):    # check number
+        print('Число четное!')
+    else:
+        print('Число нечетное!')
+
+
+#============================================================
+
+class Fifo_bufer: # first way
+    def __init__(self, long_n):
+        self.bufer = [0] * long_n
+
+    def buferUse(self, new_value):
+        self.bufer = self.bufer[1:] + [new_value]#
+
+first_fifo_bufer = Fifo_bufer(7)
+first_fifo_bufer.buferUse(6)
+
+print(first_fifo_bufer.bufer)
+
+#=============================================================
+
+class Fifo_bufer_two: # second way
+    def __init__(self, long_n):
+        self.bufer = [0] * long_n
+
+    def buferUse(self, new_value):
+        self.bufer.pop(0)
+        self.bufer.append(new_value)
+        
+
+fifo = Fifo_bufer_two(5)
+fifo.buferUse(5)
+print(fifo.bufer)
+
+
+#=================================================================
+
+from collections import deque as dq
+
+class Fifo_bufer_three: # thirt way
+    def __init__(self, long_n):
+        self.bufer = dq([0 for i in range(long_n)], maxlen=long_n)
+
+    def buferUse(self, new_value):
+        self.bufer.append(new_value)
+
+
+fifo_three = Fifo_bufer_three(5)
+fifo_three.buferUse(5)
+
+print(fifo_three.bufer)
+
+
+#================================================================
+
+
+def bubleSort(nums): # Сортировка пузырьком. Выбрал ее потому что, на данный момент изучил только его.
+    check = True
+    while check:
+        check = False
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]:
+                nums[i], nums[i+1] = nums[i+1], nums[i]
+                check = True
+    return nums
+
+
+random_list_of_nums = [5, 2, 1, 8, 4]  
+
+print(bubleSort(random_list_of_nums))
